be79c5c44610ce116e84811f5424dea0352f759d
donell <donell.wambua@student.moringaschool.com>
2023-06-21 15:35:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+main.remove()
+const newHeader=document.createElement("h1")
+newHeader.setAttribute("id", "victory")
+
+newHeader.id = 'victory'
+newHeader.textContent = 'Migan is the champion'
